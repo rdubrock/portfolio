1b1dd50ecba3982f91a482b46ebf146330c0072f
Russell DuBrock <russell.dubrock@gmail.com>
2015-05-14 12:14:39 -0700

    removed js scroll from smaller screen sizes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,29 @@
 
 $( document ).ready(function() {
    
-
-  $(window).scroll(function() {
+  if (window.innerWidth >= 600) {
+    $(window).scroll(function() {
     
-    // console.log($(this).scrollHeight())
-    // var currentHeight = this.scrollHeight - this.scrollTop
-    if ($(this).scrollTop() > window.innerHeight/1.5) {
-      $('#slider').slideDown();
-      // $('#project-row').removeAttr('height');
-    }
-    if ($(this).scrollTop() < window.innerHeight/1.5) {
-      $('#slider').slideUp();
-      // $('#project-row').css('height: 40vh;')
-    }
+      // console.log($(this).scrollHeight())
+      // var currentHeight = this.scrollHeight - this.scrollTop
+      if ($(this).scrollTop() > window.innerHeight/1.5) {
+        $('#slider').slideDown();
+        // $('#project-row').removeAttr('height');
+      }
+      if ($(this).scrollTop() < window.innerHeight/1.5) {
+        $('#slider').slideUp();
+        // $('#project-row').css('height: 40vh;')
+      }
 
-    if ($(this).scrollTop() > window.innerHeight) {
-      $('.header h1').hide();
-    }
+      if ($(this).scrollTop() > window.innerHeight) {
+        $('.header h1').hide();
+      }
 
-    if ($(this).scrollTop() < window.innerHeight) {
-      $('.header h1').show();
-    }
-
-
-
-  }); 
+      if ($(this).scrollTop() < window.innerHeight) {
+        $('.header h1').show();
+      } 
+    }); 
+  }
  $('.thumbnail').hover(
         function(){
             $(this).find('.caption').slideDown(250); //.fadeIn(250)
